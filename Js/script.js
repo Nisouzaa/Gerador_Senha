35347a0f6cae28d5c101b4e2175ba181e0488b90
Nisouzaa <souzanini@gmail.com>
2026-03-10 12:35:27 -0300

    volto ja

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,11 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
-// Novas funcionalidadaes 
+// Novas funcionalidadaes
 
-const openClosegeneratorButton = document.querySelector("#open-generate-password");
+const openClosegeneratorButton = document.querySelector(
+  "#open-generate-password",
+);
 const geneatePasswordContainer = document.querySelector("#generate-options");
 const lengthInput = document.querySelector("#length");
 const lettersInput = document.querySelector("#letters");
@@ -13,67 +15,75 @@ const copyPasswordButton = document.querySelector("#copy-password");
 
 // Function to generate a random password
 const getLetterLowercase = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 
 const getLetterUppercase = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
 const getNumber = () => {
-    return Math.floor(Math.random() * 10).toString();   
+  return Math.floor(Math.random() * 10).toString();
 };
 
 const getSymbol = () => {
-    const symbols = "!@#$%^&*()_+{}[]:;<>,.?/~`|";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = "!@#$%^&*()_+{}[]:;<>,.?/~`|";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-const generatedPassword = (getLetterLowercase, getLetterUppercase, getNumber, getSymbol) => {
-    let password = "";
+const generatedPassword = (
+  getLetterLowercase,
+  getLetterUppercase,
+  getNumber,
+  getSymbol,
+) => {
+  let password = "";
 
-    const passwordLength = lengthInput.value;
+  const passwordLength = +lengthInput.value;
 
-    const generators = [];   
+  const generators = [];
 
-    if (lettersInput.checked) {
-        generators.push(getLetterLowercase, getLetterUppercase);
-    }
+  if (lettersInput.checked) {
+    generators.push(getLetterLowercase, getLetterUppercase);
+  }
 
-    if (numbersInput.checked) {
-        generators.push(getNumber);
-    }
+  if (numbersInput.checked) {
+    generators.push(getNumber);
+  }
 
-    if (symbolsInput.checked) {
-        generators.push(getSymbol);
-    }
+  if (symbolsInput.checked) {
+    generators.push(getSymbol);
+  }
 
-    if (generators.length === 0) {
-        return;
-    }
+  if (generators.length === 0) {
+    return;
+  }
 
-    
-    for (let i = 0; i < passwordLength; i = i + 4) {
-        generators.forEach(() => {
-            const randomValue = generators[Math.floor(Math.random() * generators.length)];
-            password += randomValue();
-        });
-    };
+  for (let i = 0; i < passwordLength; i = i + generators.length) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)];
+      password += randomValue();
+    });
+  }
 
-    password = password.slice(0, passwordLength);
+  password = password.slice(0, passwordLength);
 
-    generatedPasswordElement.style.display = "block";
-    generatedPasswordElement.querySelector("h4").innerText = password;
-
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
 };
-
-
 
 // Event listener for the generate password button
-generatePasswordButton.addEventListener("click", () => {
-    generatedPassword(getLetterLowercase, getLetterUppercase, getNumber, getSymbol);
+generatePasswordButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  generatedPassword(
+    getLetterLowercase,
+    getLetterUppercase,
+    getNumber,
+    getSymbol,
+  );
 });
 
 openClosegeneratorButton.addEventListener("click", () => {
-    geneatePasswordContainer.classList.toggle("hide");
+  geneatePasswordContainer.classList.toggle("hide");
 });
